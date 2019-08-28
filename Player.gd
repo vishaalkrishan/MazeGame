39d6725207f09a635de5eb5cb71a93cc18463c7b
SpruceKETS <39812473+SpruceKTS@users.noreply.github.com>
2019-08-27 17:24:02 -0700

    Initial Commit

---
+++ Player.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2()
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	
+	if velocity.length() > 0 :
+		if Input.is_action_pressed("ui_accept"):
+			velocity = velocity.normalized() * 350
+		else:
+			velocity = velocity.normalized() * 150
+			
+	position += velocity * delta
